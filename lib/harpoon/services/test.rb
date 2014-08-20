c9d80b1edeb8e19c354dcf7ff05758dff56f05bb
Ryan <ryan@mazondo.com>
2014-08-19 20:27:57 -0600

    test service created

--- lib/harpoon/services/test.rb
+++ lib/harpoon/services/test.rb
@@ -1,26 +1,15 @@
 module Harpoon
 	module Services
 		class Test
-			attr_accessor :uploads, :dns_changes
-			attr_reader :auth
+			attr_accessor :options, :requests
+
 			def initialize(options = {})
-				@auth = auth
-				@options = options.config
+				@options = options
+				@requests = []
 			end
 
-			def upload(location, files = [], options = {})
-				self.uploads.push({location: location, files: files, options: options})
-				return "Fake deploy successful"
-			end
-
-			def create_dns(url)
-				self.dns_changes << {type: "create", info: method(__method__).parameters}
-				return true
-			end
-
-			def forward_dns(from, to)
-				self.dns_changes << {type: "forward", info: method(__method__).parameters}
-				return true
+			def method_missing(method, *args)
+				@requests.push [method, args]
 			end
 		end
 	end
